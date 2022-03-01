310ffb86c0b31e67de24507e93734c7901e05126
Nihat Farz <99778063+NihatFarz@users.noreply.github.com>
2022-03-02 03:06:20 +0400

    Create farz.sh

---
+++ farz.sh
@@ -0,0 +1,50 @@
+echo
+echo -e "\e[1m Termux Qurulumu Başlayır...!!!"
+apt update && apt upgrade -y
+apt-get install python -y
+apt-get install python2 -y
+apt-get install python-pip -y
+	pip install wordlist -y
+apt-get install sudo -y
+apt-get install tsu -y
+apt-get install proot -y
+apt-get install pv -y
+apt-get install ruby -y
+apt-get install cat -y
+apt-get install screenfetch -y
+apt-get install cowsay -y
+apt-get install toilet -y
+apt-get install figlet -y
+apt-get install php -y
+apt-get install perl -y
+apt-get install nmap -y
+apt-get install bash -y
+apt-get install nano -y
+apt-get install curl -y
+apt-get install tar -y
+apt-get install zip -y
+apt-get install fish -y
+apt-get install unzip -y
+apt-get install wget -y
+apt-get install cmatrix -y
+apt-get install openssl -y
+apt-get install openssh -y
+apt-get install ngrok -y
+apt-get install wget -y
+apt-get install curl -y
+pip install grip #Markdown Oxumaq üçün // grip -b *.md
+pip3 install pysocks bs4 scapy-python3 #Hibernet Botnet requirements
+apt-get install nmap hydra medusa ncrack #Brutedum requirements
+	#One-Lin3r
+	pkg install nano -y
+	pkg install git -y
+	pkg install perl -y
+	pkg install python -y
+	pkg install python2 -y
+	pip install --upgrade pip
+	python3 -m pip install --upgrade pip
+	pip3 install one-lin3r
+	#One-Lin3r
+  
+echo
+echo -e "\e[1m\e[32m Qurulum Bitdi. :\e[33m Developer NihatFarz"  
