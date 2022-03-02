2bee5894acb10e4c6d3f7e82826ca04feb80ba98
Nihat Farz <99778063+NihatFarz@users.noreply.github.com>
2022-03-03 03:42:56 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ echo '
 apt update && apt upgrade -y
 
 echo
-echo -e "\e[1m\e[32m Termux Qurulumu  Uğurla Tamamlandı. \e[1m\e[32m Developer NihatFarz" 
+echo -e "\e[1m\e[32m Termux Qurulumu Uğurla Tamamlandı.\e[1m\e[32m Developer NihatFarz" 
 
 echo
 echo -e "\e[1m\e[32m exit Yazaraq çıxış edib təkrar Termux a giriş edin"
