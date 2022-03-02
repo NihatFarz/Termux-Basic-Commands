7eb29dfc0c4c45d94fcda356b882415c42a6619e
Nihat Farz <99778063+NihatFarz@users.noreply.github.com>
2022-03-03 03:35:23 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ echo -e "\e[1m Termux Qurulumu Başlayır...!!!"
 apt update && apt upgrade -y
 
 echo
-echo -e "\e[1m\e[32m Qurulum Bitdi. 1m\e[33m Developer NihatFarz" 
+echo -e "\e[1m\e[32m Qurulum Bitdi. \e[1m\e[32m Developer NihatFarz" 
 
 echo 
 echo '            
