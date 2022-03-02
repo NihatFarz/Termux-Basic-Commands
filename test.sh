a81f3bfbed3e617040a407750db0fdcf80ca0e76
Nihat Farz <99778063+NihatFarz@users.noreply.github.com>
2022-03-03 03:30:06 +0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+echo
+echo -e "\e[1m Termux Qurulumu Başlayır...!!!"
+apt update && apt upgrade -y
+
+echo
+echo -e "\e[1m\e[32m Qurulum Bitdi. :\e[33m Developer NihatFarz" 
+
+echo 
+echo '            
+          ███████╗ █████╗ ██████╗ ███████╗
+          ██╔════╝██╔══██╗██╔══██╗╚══███╔╝
+          █████╗  ███████║██████╔╝  ███╔╝ 
+          ██╔══╝  ██╔══██║██╔══██╗ ███╔╝  
+          ██║     ██║  ██║██║  ██║███████╗
+          ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝'      
+          
+          
+echo           
+echo -e "Telegram @NihatFarz"
