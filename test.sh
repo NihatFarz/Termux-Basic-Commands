8b39f7e79ad6c3d1db5c558499ecb38714dd38d0
Nihat Farz <99778063+NihatFarz@users.noreply.github.com>
2022-03-03 03:45:55 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,11 +5,15 @@ echo '
           █████╗  ███████║██████╔╝  ███╔╝ 
           ██╔══╝  ██╔══██║██╔══██╗ ███╔╝  
           ██║     ██║  ██║██║  ██║███████╗
-          ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝'     
+          ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝' 
+  
+echo           
+echo -e "\e[1m\e[32m Termux Qurulumu Başladıldılır...."
+
 apt update && apt upgrade -y
 
 echo
-echo -e "\e[1m\e[32m Termux Qurulumu Uğurla Tamamlandı.\e[1m\e[32m Developer NihatFarz" 
+echo -e "\e[1m\e[32m Termux Qurulumu Uğurla Tamamlandı." 
 
 echo
 echo -e "\e[1m\e[32m exit Yazaraq çıxış edib təkrar Termux a giriş edin"
@@ -25,3 +29,9 @@ echo '
           
 echo           
 echo -e "Telegram @NihatFarz"
+
+echo           
+echo -e " "
+
+echo           
+echo -e " "
